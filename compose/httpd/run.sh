24647d95628be35a618c3778ad98502ac2941d0a
Federica Agostini <federica.agostini@cnaf.infn.it>
2025-06-19 19:32:53 +0200

    Install apache on debian

---
+++ compose/httpd/run.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+apt update
+apt install -y apache2 libapache2-mod-auth-openidc
+
+a2enmod auth_openidc cgi
+a2enconf openidc
+
+apt install -y curl
+# This is required since the curl installation has overwritten
+# the CA bundle including igi-tes-tca
+cp /debian-ssl/ca-certificates.crt /etc/ssl/certs/ca-certificates.crt 
+
+apache2ctl -D FOREGROUND -e debug
