38d1868536c112d43ce9050d549d4049bfa7e286
enricovianello <maverick17586@gmail.com>
2026-03-17 18:14:05 +0100

    Fix sonar issues

--- esaco-app/src/main/java/it/infn/mw/esaco/config/SecurityConfig.java
+++ esaco-app/src/main/java/it/infn/mw/esaco/config/SecurityConfig.java
@@ -27,7 +27,7 @@ public class SecurityConfig {
   }
 
   @Bean
-  SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+  SecurityFilterChain securityFilterChain(HttpSecurity http) {
     http
       .authorizeHttpRequests(auth -> auth.requestMatchers(EndpointRequest.to("health", "info"))
         .permitAll()
